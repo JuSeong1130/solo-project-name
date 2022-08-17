492d12104a7377c08ddf18dd266e2c2f42fd0545
JuSeong1130 <jsmoon1130@naver.com>
2022-08-17 09:25:21 +0900

    Initial commit

---
+++ src/main/java/com/example/SoloProjectNameApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectNameApplication.class, args);
+	}
+
+}
